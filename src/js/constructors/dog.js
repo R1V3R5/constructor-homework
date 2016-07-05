e6008cc64611c37cdb3e0e24e2d2c4c3853b6bdd
Joshua Rivers <joshua.rivers@gmail.com>
2016-07-05 14:35:56 -0400

    have all but 2 passing.

--- src/js/constructors/dog.js
+++ src/js/constructors/dog.js
@@ -1,2 +1,14 @@
-let Dog = function () {}
-export default Dog;
+let Dog = function(color, hungry) {
+  this.status = "normal",
+  this.color = "black"
+  if (hungry !== undefined) {
+    this.hungry = hungry;
+  } else {
+    this.hungry = false;
+  }
+}
+
+// var sadie = new Dog()
+// var moonshine = new Dog()
+export
+default Dog;
--- src/js/constructors/human.js
+++ src/js/constructors/human.js
@@ -1,2 +1,14 @@
-let Human = function () {}
-export default Human;
+let Human = function() {
+  this.cool = true
+  this.pet = function(dog) {
+    dog.status = 'happy';
+  }
+  this.feed = function(dog) {
+    dog.hungry = false;
+  }
+}
+
+// var mason = new Human();
+// var julia = new Human();
+export
+default Human;
